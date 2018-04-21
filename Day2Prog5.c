7db38dd86342c173caba76a1d6b15e223b542657
rishtech21 <rishabhmadan17@gmail.com>
2018-04-21 03:35:24 -0700

    Relational Operators

---
+++ Day2Prog5.c
@@ -0,0 +1,15 @@
+/*
+*
+* Author - Rishabh Madan
+* Problem - Relational Operators ==> '>', '<', '<=', '>=', '==', '!='
+* Day 2 Program 5
+*
+*/
+#include<stdio.h>
+
+int main(){
+    printf("%d\n", printf("Hello") == printf("Hello"));
+    printf("%d\n", printf("Angel") == printf("Deamon"));
+
+    return 0;
+}
